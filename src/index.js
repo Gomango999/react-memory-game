c50a473c713213b08878c41c02fd688cb2c681fc
Gomango999 <kv.zhu999@gmail.com>
2020-05-22 13:55:40 +1000

    Added Static Website

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card-container">
+        <div className="card"></div>
+      </div>
+    );
+  }
+}
+
+class Board extends React.Component {
+  render () {
+    let numCards = this.props.rows * this.props.cols;
+    let board = [];
+    for (let y = 0; y < this.props.rows; y++) {
+      let row = []
+      for (let x = 0; x < this.props.cols; x++) {
+        row.push(<Card key={x}/>);
+      }
+      board.push(
+        <div className="board-row" key={y}>
+          {row}
+        </div>
+      );
+    }
+    return (
+      <div>
+        {board}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    let status = "Match cards to win!"
+    return (<div className="game">
+      {/* <h1>Memory</h1> */}
+      <div className="board">
+        <Board
+          rows={3}
+          cols={6}
+        />
+      </div>
+      <div className="status">
+        {status}
+        <br/>
+        <button className="reset">Reset</button>
+      </div>
+    </div>);
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
